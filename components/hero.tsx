a6d556d7991f3a06fff5355d7cbd6a5055c60e55
Vikram Vaibhav <vikramvaibhav@Vikrams-MacBook-Air.local>
2025-04-02 16:49:22 +1100

    FEA-003 responsive UI all devices

--- components/hero.tsx
+++ components/hero.tsx
@@ -32,16 +32,16 @@ export function Hero() {
         ease={50}
       />
       <div className="absolute inset-0 z-20 bg-gradient-to-t from-background to-background/60 dark:from-background dark:to-background/60" />
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center flex-col-reverse md:flex-row">
       <motion.div
-        className="relative z-30 mx-auto max-w-5xl text-center"
+        className="relative z-30 mx-auto max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-5xl text-center"
         style={{ y: textY }}
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: "easeOut" }}
       >
         <motion.h1
-          className="bg-gradient-to-b from-black to-black/50 dark:from-white dark:to-white/50 bg-clip-text text-4xl font-bold text-transparent sm:text-5xl md:text-6xl lg:text-7xl"
+          className="bg-gradient-to-b from-black to-black/50 dark:from-white dark:to-white/50 bg-clip-text text-4xl font-bold text-transparent sm:text-5xl md:text-6xl lg:text-7xl pb-2"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.2, ease: "easeOut" }}
@@ -74,7 +74,7 @@ export function Hero() {
         </motion.div>
       </motion.div>
       <motion.div 
-        className="relative z-30 mx-auto max-w-5xl text-center"
+        className="relative z-30 mx-auto max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-5xl text-center"
         style={{ y: roverY }}
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
